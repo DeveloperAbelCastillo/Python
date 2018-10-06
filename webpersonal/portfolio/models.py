3f36883eaeac11a177ae75393c18c6c68d5e8c99
Abel Castillo <abcalajer@gmail.com>
2018-10-06 18:19:55 -0500

    Adecuacion portfolio
    Se adecuo portfolio para la creacion y muestra de proyectos

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -4,9 +4,10 @@ from django.db import models
 class ProjectPortfolio(models.Model):
     title = models.CharField(max_length=200, verbose_name = 'Título')
     description = models.TextField(verbose_name = 'Descripcion')
-    startOfEmployment = models.DateField(verbose_name = "Inicio de Empleo")
-    exitOfEmployment = models.DateField(null=True, blank=True, verbose_name="Salida de Empleo")
+    startproject = models.DateField(verbose_name = "Inicio de Empleo")
+    exitproject = models.DateField(null=True, blank=True, verbose_name="Salida de Empleo")
     image = models.ImageField(verbose_name = 'Imagen', upload_to="projectsportfolio")
+    urlproject = models.CharField(null=True, blank=True, max_length=500, verbose_name = 'Url')
     created = models.DateTimeField(auto_now_add=True, verbose_name = 'Fecha de Creación')
     updated = models.DateTimeField(auto_now=True, verbose_name = 'Fecha de Actualización')
 
